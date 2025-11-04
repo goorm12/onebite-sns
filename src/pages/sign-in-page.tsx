6eeb8f5954ea75513fe4ad655ab1d59a3f93df0b
빈운기 <vinwoonki98@gmail.com>
2025-11-04 09:41:40 +0900

    feat : 로그인, 회원가입 기능 구현 라우트 가드 구현

--- src/pages/sign-in-page.tsx
+++ src/pages/sign-in-page.tsx
@@ -1,3 +1,91 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useSignInWithPassword } from "@/hooks/mutations/use-sign-in-with-password";
+
+import { useState } from "react";
+import { Link } from "react-router";
+
+import githubLogo from "@/assets/github-mark.svg";
+import { useSignInWithOAuth } from "@/hooks/mutations/use-sign-in-with-oauth";
+import { toast } from "sonner";
+import { generateErrorMessage } from "@/lib/error";
+
 export default function SignInPage() {
-  return <div>SignInPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { mutate: signInWithPassword, isPending: isSignInWithPasswordPending } =
+    useSignInWithPassword({
+      onError: (error) => {
+        const message = generateErrorMessage(error);
+        toast.error(message);
+        setPassword("");
+      },
+    });
+  const { mutate: signInWithOAuth, isPending: isSignInWithOAuthPending } =
+    useSignInWithOAuth({
+      onError: (error) => {
+        const message = generateErrorMessage(error);
+        toast.error(message);
+      },
+    });
+
+  const handleSignInWithPassword = () => {
+    if (email.trim() === "") return;
+    if (email.trim() === "") return;
+
+    signInWithPassword({ email, password });
+  };
+
+  const handleSignInWithGitHubClick = () => {
+    signInWithOAuth("github");
+  };
+
+  const isPending = isSignInWithOAuthPending || isSignInWithPasswordPending;
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="text-xl font-bold">로그인</div>
+      <div className="flex flex-col gap-2">
+        <Input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="py-6"
+          type="email"
+          placeholder="example@abc.com"
+          disabled={isPending}
+        />
+        <Input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="py-6"
+          type="password"
+          placeholder="password"
+          disabled={isPending}
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <Button
+          className="w-full"
+          onClick={handleSignInWithPassword}
+          disabled={isPending}
+        >
+          로그인
+        </Button>
+        <Button
+          className="w-full"
+          variant={"outline"}
+          onClick={handleSignInWithGitHubClick}
+          disabled={isPending}
+        >
+          <img src={githubLogo} className="h-4 w-4" />
+          GitHub 계정으로 로그인
+        </Button>
+      </div>
+      <div>
+        <Link className="text-muted-foreground hover:underline" to={"/sign-up"}>
+          계정이 없으시다면? 회원가입
+        </Link>
+      </div>
+    </div>
+  );
 }
